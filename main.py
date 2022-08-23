c428e3f3ccaf372cb10f7c02da52cb588c3714a8
1367227192 <52883931+1367227192@users.noreply.github.com>
2022-08-24 05:04:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ data = {
         "date":{"value":dates,"color":get_random_color()},
   
         "love_days": {"value":get_count(),"color":get_random_color()},
-        "birthday_left":{"value":get_birthday(), "color":get_random_color()}},
+        "birthday_left": {"value":get_birthday(), "color":get_random_color()},
         "words": {"value":get_words(),"color":get_random_color()},
         }
 count = 0
